286ba8890c74bb9aaf8a27f09b4b377825ab80fc
kibbles <c4wfee@gmail.com>
2020-05-18 22:20:15 -0400

    Added "--today" flag to grab subtitles for today's episode

--- subsnow.py
+++ subsnow.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Subtitles Now is a subtitle downloader for Democracy Now! .mp4 videos.
+# Subtitles Now!
+# A subtitle downloader for Democracy Now! offline videos. It grabs the subtitles from the live episode
+# on their website and converts them into .srt format, which is compatible with most media players.
 
 from sys import argv
 from os import remove
+from datetime import date
 
 import youtube_dl
 import webvtt
@@ -12,21 +15,26 @@ import webvtt
 
 if __name__ == "__main__":
     if len(argv) > 1:
-        video_url = argv[1]
+        if argv[1].strip() == "--today":
+            # The "--today" flag grabs the episode for the current date based on system time
+            today = date.today()
+            video_url = "https://www.democracynow.org/shows/" + today.strftime("%Y/%m/%d")
+        else:
+            video_url = argv[1]
     else:
-        video_url = input("Copy and paste a Democracy Now video URL: ")
+        video_url = input("Copy and paste a Democracy Now Full Episode video URL:\n")
 
     video_url = video_url.strip()
 
     # Match the Democracy Now video filenames (ex. dn2020-0515.mp4)
     video_id = video_url.split("/")
-    video_date_month = int(video_id[5])
-    video_date_day = int(video_id[6])
+    video_date_month = video_id[5]
+    video_date_day = video_id[6]
 
     # Pad the day and month with a zero if necessary.
-    if video_date_month < 10:
+    if int(video_date_month) < 10 and not video_date_month[0] == "0":
         video_date_month = "0" + video_id[5]
-    if video_date_day < 10:
+    if int(video_date_day) < 10 and not video_date_day[0] == "0":
         video_date_day = "0" + video_id[6]
     video_id = "dn", str(video_id[4]), "-", str(video_date_month), str(video_date_day)
     video_id = (''.join(video_id))
@@ -43,7 +51,6 @@ if __name__ == "__main__":
         ydl.download([video_url])
 
     # Convert the .vtt file to .srt
-
     vtt_filepath = video_id + ".en.vtt"
 
     vtt = webvtt.read(vtt_filepath)
